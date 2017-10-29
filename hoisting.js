dc390b9a5f03a40f7f15e4cd02ca8de6dc1d60a4
Elise Bonner <elise0209@mac.com>
2017-10-29 00:30:23 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -5,16 +5,17 @@ function callMe() {
   console.log("so call me");
   var lyric = "maybe";
   return lyric;
-  
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
+    thisIsCrazy();
   }
 }
 
